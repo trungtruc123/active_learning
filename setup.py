291130c5173aeba65c29c3b37f3eee6c6a8ecbf9
tructt <tructt@ftech.ai>
2022-07-26 15:54:42 +0700

    init commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="active_learning",
+    version="0.0.1",
+    author="Tran Trung Truc",
+    author_email="tieuthu.anninh@gmail.com",
+    description="active_learning for NLP",
+    long_description="Full description",
+    url="https://github.com/trungtruc123/active_learning.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
